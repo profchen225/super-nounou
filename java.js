c4b794e3d8470cbd4443866086defb1ec17ce37b
profchen225 <163119556+profchen225@users.noreply.github.com>
2024-04-22 08:41:53 +0000

    Delete java.js

--- java.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const btn = document.querySelector(".btn");
-    
-    btn.addEventListener("click", function() {
-        alert("Bienvenue chez Super Nounou !");
-    });
-});
